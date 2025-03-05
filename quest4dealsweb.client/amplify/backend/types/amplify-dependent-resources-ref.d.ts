f4c7be7760c67f30faca086216bd3f9320c2733a
Oscar Ruelas <oruelas@ewu.edu>
2025-03-05 09:28:41 -0800

    added some amplify configuration settings

---
+++ quest4dealsweb.client/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "quest4dealswebclient27793166": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
