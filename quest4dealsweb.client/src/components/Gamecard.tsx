7ea0ea8b495f1d8937d91d5974b0b6bbf4ceb0c6
Ryan Hirte <rhirte@ewu.edu>
2025-03-11 16:34:35 -0700

    fix hopefully

--- quest4dealsweb.client/src/components/Gamecard.tsx
+++ quest4dealsweb.client/src/components/Gamecard.tsx
@@ -1,11 +1,25 @@
-function Gamecard() {
+// Inner part of Game interface because it is another object inside the Game interface for JSON data
+interface GameInfo {
+  lowest_price: number;
+  short_desc: string;
+}
+
+// Game interface for JSON data
+interface Game {
+  image: string;
+  title: string;
+  game_info: GameInfo;
+}
+
+function Gamecard({ game }: { game: Game }) {
+  // This is accessing the game object passed as a prop to the Gamecard component and
+  // displaying the image, title, lowest price, and short description of the game in the game card
   return (
     <div className="gamecard">
-      <img src="" alt="Game Image" />
-      <h2>Title</h2>
-      <p>Price</p>
-      <p>Rating</p>
-      <p>Short Description</p>
+      <img src={game.image} alt="Game Image" />
+      <h2>{game.title}</h2>
+      <p>Lowest Price: ${game.game_info.lowest_price}</p>
+      <p className="desc">{game.game_info.short_desc}</p>
     </div>
   );
 }
