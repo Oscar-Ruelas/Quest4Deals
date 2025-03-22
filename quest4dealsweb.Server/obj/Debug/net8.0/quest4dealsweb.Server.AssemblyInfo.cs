e39d01523d4fc1ca4df4d0688bfe48c0ef7d5ba8
Oscar Ruelas <oruelas@ewu.edu>
2025-03-21 23:55:31 -0700

    Removed unnecessary files and dependencies

--- quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
+++ quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05343bf9cf486ba1369f8281d82b82e75d970cb8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b01f1845857d150ea8029beb98ebac92a894f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
