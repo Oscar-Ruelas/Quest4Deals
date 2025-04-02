2ea6e3780cc15e693b386a8f59b9ab14d4c91a43
Oscar Ruelas <114115100+Oscar-Ruelas@users.noreply.github.com>
2025-04-02 11:37:32 -0700

    Update Register.tsx

--- quest4dealsweb.client/src/pages/Register.tsx
+++ quest4dealsweb.client/src/pages/Register.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import "../styling/register.css";
+import "../styling/Register.css";
 
 function Register() {
     const [name, setName] = useState("");
