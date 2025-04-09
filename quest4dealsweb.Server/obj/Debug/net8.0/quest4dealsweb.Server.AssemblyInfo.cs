4149a9abe8e05d13eebd5d097078425b2bf86d43
Oscar Ruelas <oruelas@ewu.edu>
2025-04-09 16:40:46 -0700

    Done for today

--- quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
+++ quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e55d72d2288be5fc6f3295103a9a44c202ead4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87b5d3becf6a4850a678059df5588a871443f764")]
 [assembly: System.Reflection.AssemblyProductAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
