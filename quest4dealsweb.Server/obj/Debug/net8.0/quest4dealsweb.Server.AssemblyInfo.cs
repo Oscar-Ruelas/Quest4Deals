b951416a4d1329151bc7595609f6449d3db4c4f1
afudge1 <166938653+afudge1@users.noreply.github.com>
2025-05-22 19:23:52 -0700

    test

--- quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
+++ quest4dealsweb.Server/obj/Debug/net8.0/quest4dealsweb.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e18ad44706737d81412407279e11778df310c7b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5172108839d070c6b26831721377d2a9733418c")]
 [assembly: System.Reflection.AssemblyProductAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("quest4dealsweb.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
